6f6ce19ccce8981a45db4f76ac92a70fd52a92c9
Moaaad99 <89322325+Moaaad99@users.noreply.github.com>
2022-02-04 13:45:15 +0100

    Lite fix

--- SpelProjekt/Assets/Moa/MusicPrefabScripts/AudioManager.cs
+++ SpelProjekt/Assets/Moa/MusicPrefabScripts/AudioManager.cs
@@ -1,9 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
+
+
+    //Synligt i Fmod
+    [Header("Player")]
+
+    [EventRef]
+    public string playerFootsteps;
+    [EventRef]
+    public string playerJump;
+    [EventRef]
+    public string playerLand;
+    [EventRef]
+    public string playerHurt;
+
+
+    //ljud
+    [Header("The Monsters")]
+
+    [EventRef]
+    public string MonsterA;
+    [EventRef]
+    public string MonsterB;
+    [EventRef]
+    public string MonsterC;
+    [EventRef]
+    public string MonsterD;
+    [EventRef]
+    public string MonsterE;
+    [EventRef]
+    public string MonsterF;
+
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
