a7e2a52ec28607820e7f2a413d393e1675becc23
MitchooKvirre <johan_kvist@hotmail.se>
2022-02-21 11:35:23 +0100

    QuestTrigger

--- SpelProjekt/Assets/Johan/SCRIPTS/Interactable/QuestTrigger_Interractable.cs
+++ SpelProjekt/Assets/Johan/SCRIPTS/Interactable/QuestTrigger_Interractable.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(QuestGiver))]
 public class QuestTrigger_Interractable : Interactable
 {
 
