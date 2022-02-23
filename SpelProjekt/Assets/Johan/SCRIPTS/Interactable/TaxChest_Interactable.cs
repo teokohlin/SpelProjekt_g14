e9032822f5db7c00f6eab584d66978b6a7d8b597
Fodonzo <38867714+Fodonzo@users.noreply.github.com>
2022-02-23 10:59:16 +0100

    main scen och script

--- SpelProjekt/Assets/Johan/SCRIPTS/Interactable/TaxChest_Interactable.cs
+++ SpelProjekt/Assets/Johan/SCRIPTS/Interactable/TaxChest_Interactable.cs
@@ -8,9 +8,11 @@ public class TaxChest_Interactable : Interactable
     public GameObject taxCanvas;
     private TaxChest taxChest;
     private PlayerController pc;
+    private DialogueTrigger trigger;
 
     private void Start()
     {
+        trigger = GetComponent<DialogueTrigger>();
         taxChest = GetComponent<TaxChest>();
         pc = FindObjectOfType<PlayerController>();
     }
@@ -29,6 +31,7 @@ public class TaxChest_Interactable : Interactable
 
     public override void InteractWith(PlayerController pc)
     {
+        trigger.TriggerDialogue();
         if (taxCanvas.activeSelf == false)
         {
             taxCanvas.SetActive(true);
