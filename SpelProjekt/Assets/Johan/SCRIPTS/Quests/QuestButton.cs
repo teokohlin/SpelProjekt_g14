e66d14d98f5fe9c07c980f48596c5764db9ebc63
MitchooKvirre <johan_kvist@hotmail.se>
2022-03-04 16:41:44 +0100

    QuestPanel med flera goals

--- SpelProjekt/Assets/Johan/SCRIPTS/Quests/QuestButton.cs
+++ SpelProjekt/Assets/Johan/SCRIPTS/Quests/QuestButton.cs
@@ -11,9 +11,11 @@ public class QuestButton : MonoBehaviour
     public Quest quest;
 
     public TextMeshProUGUI titleText;
-    public TextMeshProUGUI progressText;
-    public Image requiredResourceImage;
-    public GameObject[] progressObjects;
+    //public TextMeshProUGUI progressText;
+    //public Image requiredResourceImage;
+    public GameObject panelInPanel;
+    public GameObject progressPrefab;
+    public List<GameObject> progressObjects = new List<GameObject>();
 
     private void Start()
     {
@@ -21,29 +23,48 @@ public class QuestButton : MonoBehaviour
         titleText.text = quest.title;
         for (int i = 0; i < quest.goals.Length; i++)
         {
-            //progressObjects
+            //progressObjects[i].GetComponentInChildren<TextMeshProUGUI>()
+            GameObject progressObj = Instantiate(progressPrefab, panelInPanel.transform.position, Quaternion.identity, panelInPanel.transform);
+            //progressObj.transform.parent = panelInPanel.transform;
+            progressObjects.Add(progressObj);
         }
-        progressText.text = quest.goals[0].currentAmount.ToString() +  "/" + quest.goals[0].requiredAmount.ToString();
-        requiredResourceImage.sprite = quest.goals[0].requiredResourceSprite;
+        //progressText.text = quest.goals[0].currentAmount.ToString() +  "/" + quest.goals[0].requiredAmount.ToString();
+        //requiredResourceImage.sprite = quest.goals[0].requiredResourceSprite;
 
-        //if ()
-        //{
-
-        //}
     }
     private void Update()
     {
         titleText.text = quest.title;
-        progressText.text = quest.goals[0].currentAmount.ToString() +  "/" + quest.goals[0].requiredAmount.ToString();
-        requiredResourceImage.sprite = quest.goals[0].requiredResourceSprite;
-        if (quest.goals[0].requiredResourceSprite == null)
+
+        for (int i = 0; i < progressObjects.Count; i++)
         {
-            requiredResourceImage.gameObject.SetActive(false);
+            progressObjects[i].GetComponentInChildren<TextMeshProUGUI>().text =
+                quest.goals[i].currentAmount.ToString() +  "/" + quest.goals[i].requiredAmount.ToString();
+            progressObjects[i].GetComponentInChildren<Image>().sprite =
+                quest.goals[i].requiredResourceSprite;
+            if (quest.goals[i].requiredResourceSprite == null)
+            {
+                 progressObjects[i].GetComponentInChildren<Image>().gameObject.SetActive(false);
+            }
+            else
+            {
+                progressObjects[i].GetComponentInChildren<Image>().gameObject.SetActive(true);
+            }
+
         }
-        else
-        {
-            requiredResourceImage.gameObject.SetActive(true);
-        }
+
+
+        //progressText.text = quest.goals[0].currentAmount.ToString() +  "/" + quest.goals[0].requiredAmount.ToString();
+        //requiredResourceImage.sprite = quest.goals[0].requiredResourceSprite;
+        //if (quest.goals[0].requiredResourceSprite == null)
+        //{
+        //    requiredResourceImage.gameObject.SetActive(false);
+        //}
+        //else
+        //{
+        //    requiredResourceImage.gameObject.SetActive(true);
+        //}
+
 
 
     }
