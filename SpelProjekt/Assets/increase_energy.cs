0e634bb974b2cc361802725ac512a9b4eadbf734
Fodonzo <38867714+Fodonzo@users.noreply.github.com>
2022-03-05 13:26:12 +0100

    increase energy script funkar fortfarnde inte

--- SpelProjekt/Assets/increase_energy.cs
+++ SpelProjekt/Assets/increase_energy.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class increase_energy : MonoBehaviour
 {
     public Player player;
+
     public int energy_increase = 10;
-    // Start is called before the first frame update
-    void OnEnabled()
+    public void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+    public void OnEnabled()
     {
         player.maxEnergy += energy_increase;
     }
 
-    // Update is called once per frame
-    void Start()
-    {
-        player = FindObjectOfType<Player>();
-    }
 }
