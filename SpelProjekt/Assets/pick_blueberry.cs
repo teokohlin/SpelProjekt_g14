996fbeed7bdfb50e5353b71b94d972d4bbe83e00
Fodonzo <38867714+Fodonzo@users.noreply.github.com>
2022-03-07 11:57:41 +0100

    main scen blåbär funkar typ men inte riktigt

--- SpelProjekt/Assets/pick_blueberry.cs
+++ SpelProjekt/Assets/pick_blueberry.cs
@@ -5,12 +5,45 @@ using UnityEngine;
 public class pick_blueberry : Interactable
 {
     public Player player;
+    public DayNightCycle dnc;
 
     public int berryAmount = 1;
+
+    public int daysUntilGrowth;
+    private int counter;
+
+    public GameObject berrys;
+    private bool berrysActive = true;
     public override void InteractWith(PlayerController pc)
     {
         //här är allt som ska hända när man högerklickar
-        Debug.Log("Hej Teo");
-        player.AddResource(berryAmount, dType.food);
+
+        if (berrysActive == true)
+        {
+            player.AddResource(berryAmount, dType.food);
+            berrys.SetActive(false);
+            berrysActive = false;
+        }
+
+    }
+
+
+    public void Start()
+    {
+        dnc = FindObjectOfType<DayNightCycle>();
+        dnc.DayPast += Check_Day;
+        counter = daysUntilGrowth;
+    }
+    public void Check_Day()
+    {
+        if (berrysActive == false)
+        {
+            counter--;
+        }
+        else if(berrysActive == false && counter == 0) {
+            berrysActive = true;
+            counter = daysUntilGrowth;
+            berrys.SetActive(true);
+        }
     }
 }
