a5eee78f312d04215dc5173fa5a983c08befb669
MitchooKvirre <johan_kvist@hotmail.se>
2022-03-11 17:45:17 +0100

    Kanin animationer fungerar

--- SpelProjekt/Assets/Johan/SCRIPTS/FarmWorker_Worker.cs
+++ SpelProjekt/Assets/Johan/SCRIPTS/FarmWorker_Worker.cs
@@ -6,24 +6,25 @@ using UnityEngine.AI;
 
 public class FarmWorker_Worker : Worker
 {
+    public DialogueTrigger noPlownFieldsDialogueTrigger;
     private DayNightCycle Dnc;
     public Transform homePosition;
     public List<GameObject> fields = new List<GameObject>();
     //private List<GameObject> tempFields = new List<GameObject>();
-    private Vector3 currentFieldPos;
     private GameObject currentField;
-    private Vector3 newdirection;
-    private Vector3 targetDir;
     [SerializeField]
     private BoxCollider rangeBox;
     [SerializeField]
     private float animationDelay = 1;
+    [SerializeField]
+    private float animationDelay2 = 1;
 
     private bool plantedOnField; //killedwood
     private bool inAnimation = false;
 
     [Tooltip("Hur m�nga f�lt som monstret kan s� per g�ng")]
     public int targets = 10; 
+    private int originalTargets = 10;
     private Animator animator;
 
     private bool done = false;
@@ -34,19 +35,25 @@ public class FarmWorker_Worker : Worker
 
     void Start()
     {
+
+
+    }
+    private void Awake()
+    {
+        originalTargets = targets;
+        Energy = MaxEnergy; //Stor bokstav?
+
         Dnc = FindObjectOfType<DayNightCycle>();
         Dnc.DayPast += RefillEnergy;
         rangeBox = GetComponent<BoxCollider>();
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-        Debug.Log(Energy);
     }
 
 
     private void OnEnable()
     {
-        Debug.Log("Enable");
-        Energy--;
+
         List<GameObject> tempFields = new List<GameObject>(GameObject.FindGameObjectsWithTag("Field")); //Skapa tillf�llig
 
         //L�gg till alla f�lt som �r pl�jda i listan
@@ -58,14 +65,18 @@ public class FarmWorker_Worker : Worker
             }
         }
 
-        SortByDistance();
-        targets = 10;
-
         if (fields.Count <= 0)
         {
+            noPlownFieldsDialogueTrigger.TriggerDialogue();
             enabled = false;
             return;
         }
+        Energy--;
+
+        SortByDistance();
+        targets = originalTargets;
+
+
 
         currentField = fields[0];
     }
@@ -98,7 +109,7 @@ public class FarmWorker_Worker : Worker
             {
                 currentField = fields[0];
             }
-            
+            return;
         }
 
         //When fields is empty or no targets left
@@ -120,44 +131,30 @@ public class FarmWorker_Worker : Worker
 
 
 
-
-
-
-    private void GetFieldPos()
-    {
-        foreach (GameObject field in fields) //Gl�mt hur man kommer �t n�got i en list? xD
-        {
-            Vector3 vector = new Vector3(field.transform.position.x, 0.5f, field.transform.position.z);
-            currentFieldPos = vector;
-            break;
-        }
-    }
-
     void SowField()
     {
-        /*
+        
         //Trigga animation och delay
-        animator.SetTrigger("StandingUp");
-        animator.SetFloat("Speed", 0);
-        agent.speed = 0;
-        inAnimation = true;
+
         StopAllCoroutines();
         StartCoroutine("WaitOnAnimation");
-        */
-
-        
-        FieldScript field = fields[0].GetComponent<FieldScript>();
-
-        field.ChangeFarmstate();
-
-        plantedOnField = true;
-        if (plantedOnField)
-        {
-            fields.Remove(field.gameObject);
-            plantedOnField = false;
-        }
         
 
+        
+        //FieldScript field = fields[0].GetComponent<FieldScript>();
+
+        //field.ChangeFarmstate();
+
+        //plantedOnField = true;
+        //if (plantedOnField)
+        //{
+        //    fields.Remove(field.gameObject);
+        //    plantedOnField = false;
+        //}
+        
+
+        //
+        targets--;
     }
 
     private void RefillEnergy()
@@ -172,25 +169,36 @@ public class FarmWorker_Worker : Worker
         }
     }
 
-    /*
+    
     private IEnumerator WaitOnAnimation()
     {
-        yield return new WaitForSeconds(animationDelay);
-        agent.speed = 1;
-        inAnimation = false;
+        animator.SetTrigger("StandingUp");
+        animator.SetFloat("Speed", 0);
+        //agent.speed = 0;
+        inAnimation = true;
 
         FieldScript field = fields[0].GetComponent<FieldScript>();
-
-        field.ChangeFarmstate();
 
         plantedOnField = true;
         if (plantedOnField)
         {
             fields.Remove(field.gameObject);
             plantedOnField = false;
+
         }
 
 
+
+        yield return new WaitForSeconds(animationDelay);
+        
+
+        field.ChangeFarmstate(); 
+        
+        yield return new WaitForSeconds(animationDelay);
+        inAnimation = false;
+        //agent.speed = 1;
+
+
     }
-    */
+    
 }
