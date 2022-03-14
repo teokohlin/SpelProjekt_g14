6a084b8adbbe279b0677873e8e3b52b994c57d39
MitchooKvirre <johan_kvist@hotmail.se>
2022-03-14 16:21:42 +0100

    Groda med uppdrag

---
+++ SpelProjekt/Assets/Johan/SCRIPTS/Interactable/FindObject_Interactable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindObject_Interactable : Interactable
+{
+    public override void InteractWith(PlayerController pc)
+    {
+        base.InteractWith(pc);
+        Destroy(gameObject);
+    }
+}
