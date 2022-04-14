0c915acdc1be29dc08e6ff7c130e941c3084fae3
blixtcorp <jakobpersson007@gmail.com>
2022-04-14 14:03:42 +0200

    energycounting w.i.p

---
+++ SpelProjekt/Assets/energyCounter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class energyCounter : MonoBehaviour
+{
+    private Player player;
+    private int currentEnergy;
+    private int energyCap;
+    public TextMeshProUGUI energyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentEnergy = player.GetComponent<Player>().getCurrentEnergy();
+        energyCap = player.GetComponent<Player>().getMaxEnergy();
+
+        Debug.Log("current energy: " + currentEnergy);
+        Debug.Log("max energy: " + energyCap);
+    }
+}
