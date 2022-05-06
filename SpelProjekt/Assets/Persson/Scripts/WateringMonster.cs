43b03ba8ddfeca98f885f72d13ac443813aebc7c
blixtcorp <jakobpersson007@gmail.com>
2022-05-06 16:39:20 +0200

    add starting values and some base npc stuff

--- SpelProjekt/Assets/Persson/Scripts/WateringMonster.cs
+++ SpelProjekt/Assets/Persson/Scripts/WateringMonster.cs
@@ -1,18 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.AI;
 
 public class WateringMonster : MonoBehaviour
 {
+    public List<GameObject> fields = new List<GameObject>();
+    
+    public int waterCapacity;
+    public int currentWater;
+    
+    public float happiness;
+    private int maxHappiness;
+
+    public int maxEnergy;
+    public int currentEnergy;
+
+    private NavMeshAgent agent;
+    public GameObject homePosition;
+
+    public bool isActivated;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isActivated = false;
+
+        waterCapacity = 5;
+        currentWater = 5;
+
+        maxEnergy = 3;
+        currentEnergy = 3;
+
+        maxHappiness = 10;
+        happiness = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Check if the player has activated the monster before working
+        if (!isActivated)
+        {
+            return;
+        }
+        //If field list is not empty, go to the first field in the list 
+        if(fields.Count > 0)
+        {
+          
+            agent.SetDestination(fields[0].transform.position);
+        } 
+        else if(fields.Count <= 0 || targets <= 0)
+        {
+            done = true;
+
+            agent.SetDestination(homePosition.position);
+        }
+    }
+
+    void initFields()
+    {
+        // Temp list of fields
+        List<GameObject> tempFields = new List<GameObject>(GameObject.FindGameObjectsWithTag("Field")); 
+
+        //Check every field in the temp list and if they aren't watered add to another list
+        foreach (var field in tempFields)
+        {
+            //Check for sowed unwatered field tiles 
+            if (field.GetComponent<FieldScript>().ReturnFieldState() == 2)
+            {
+                fields.Add(field);
+            }
+        }
+
+
+    }
+    //Waters a field
+    void waterField(GameObject field)
+    {
         
+    }
+
+    void refillWater()
+    {
+        currentWater = waterCapacity;
     }
 }
